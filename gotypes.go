63c64058bf769d7e3da82730856075948c0172ce
Vaisakh K M <vaisakhkm2625@gmail.com>
2024-01-10 21:59:03 +0530

    var vs :=

---
+++ gotypes.go
@@ -0,0 +1,19 @@
+package main
+
+import ("fmt")
+
+var hello = "hello"
+hi := "hi" 
+
+func main() {
+var a int
+var b string 
+var c bool
+
+fmt.Println(a)
+fmt.Println(b)
+fmt.Println(c)
+    fmt.Println(hello)
+    fmt.Println(hi)
+
+}
