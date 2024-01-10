9c56786245238cf4f47e0fe61479e31616a388bd
Vaisakh K M <vaisakhkm2625@gmail.com>
2024-01-11 02:54:24 +0530

    constants

---
+++ constants.go
@@ -0,0 +1,18 @@
+package main
+
+import ("fmt")
+
+const PI = 3.14
+
+const (
+    A int = 1;
+    B int = 2;
+    C = "Hi";
+)
+
+
+func main(){
+
+    fmt.Println(PI)
+
+}
--- gotypes.go
+++ gotypes.go
@@ -3,17 +3,28 @@ package main
 import ("fmt")
 
 var hello = "hello"
-hi := "hi" 
 
 func main() {
 var a int
 var b string 
 var c bool
 
+    var d,e int = 1,2
+
+   var (
+        f int
+        g int = 1
+        h string = "hello"
+    )
+
+
+
 fmt.Println(a)
 fmt.Println(b)
 fmt.Println(c)
     fmt.Println(hello)
-    fmt.Println(hi)
+    fmt.Println(d,e)
+    fmt.Println(f,h,g)
+
 
 }
